8bbfc6e31a81badfdfde5dc21ccb5d304adb1b4c
Colin Arscott <carscott@optonline.net>
2019-03-17 13:19:51 -0400

    Update process.py

--- process.py
+++ process.py
@@ -1,13 +1,23 @@
 import Math
-data = open("kiddo.txt","r")
-hJerk = 0
-prev = 1
-current = 0
-for x in file1:
-	prev = current
-	current = x
-	if (Math.abs(double(current) - double(prev)) > hJerk):
-		hJerk = Math.abs(double(current) - double(prev)
+import time
+while True:
+	time.sleep(10)
+	data = open("kiddo.txt","r")
+	hJerk = 0
+	numShock = 0
+	prev = 1
+	current = 0
+	for x in file1:
+		prev = current
+		current = x
+		val = (Math.abs(double(current) - double(prev))
+		if val > hJerk):
+			hJerk = val
+		if val > .5:
+			hShock ++
+	j.open("data.json","w")
+	j.write("{hJerk:%d,numShock:%d}"%(hJerk,numShock))
+
 
 
 
